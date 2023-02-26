d57d523aeb747ea1dfd896553b1f83272da4cdb3
spencer-gitH <spencerdnightingale@gmail.com>
2023-02-26 08:04:40 -0500

    changed redux logging middleware to use logger

--- src/store/store.js
+++ src/store/store.js
@@ -1,28 +1,31 @@
 import { compose, createStore, applyMiddleware } from 'redux';
-// import logger from 'redux-logger';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
 
-// const loggerMiddleware =  (store) => (next) => (action) => {
-//   if(!action.type) {
-//     return next(action);
-//   }
+const persistConfig = { 
+  key: 'root',
+  storage,
+  blacklist: ['user']
+}
 
-//   console.log('type: ', action.type);
-//   console.log('payload: ', action.payload);
-//   console.log('currentState: ', store.getState());
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-//   next(action);
+const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
 
-//   console.log('next state: ', store.getState());
-// }
+const composeEnhancer = 
+  (process.env.NODE_ENV !== 'production' && 
+    window && 
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
-// const middleWares = [loggerMiddleware];
-
-// const composedEnhancers = compose(applyMiddleware(...middleWares));
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(
-  rootReducer, 
-  // undefined, 
-  // composedEnhancers
+  persistedReducer, 
+  undefined, 
+  composedEnhancers
 );
+
+export const persistor = persistStore(store);
